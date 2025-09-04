edb23e34a1f253a05adfeb88b4653527e3657d22
NoBrainer <vinniemac189@gmail.com>
2025-09-03 22:00:49 -0400

    Improved MockMvcIntegrationTest

--- src/test/java/com/example/sample_rest_app/controller/MockMvcIntegrationTest.java
+++ src/test/java/com/example/sample_rest_app/controller/MockMvcIntegrationTest.java
@@ -1,16 +1,19 @@
 package com.example.sample_rest_app.controller;
 
 import com.example.sample_rest_app.dto.PersonDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.example.sample_rest_app.util.MockUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,7 +21,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.net.URI;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -29,6 +33,9 @@ public class MockMvcIntegrationTest {
     MockMvc mockMvc;
 
     static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "c30ebd45-5e81-464b-8eeb-bef9b71404da";
 
     @DisplayName("return UNAUTHORIZED without credentials")
     @Test
@@ -47,11 +54,11 @@ public class MockMvcIntegrationTest {
      * - It is less readable.
      * - It takes seconds instead of milliseconds.
      */
-    @WithMockUser(username = "user", password = "c30ebd45-5e81-464b-8eeb-bef9b71404da")
+    @WithMockUser(username = USERNAME, password = PASSWORD)
     @DisplayName("return OK when authorized")
     @Test
     void testAuthorizedRequest() throws Exception {
-        var result = mockMvc.perform(MockMvcRequestBuilders.get(URI.create("/api/person/all")))
+        var result = mockMvc.perform(MockMvcRequestBuilders.get("/api/person/all"))
                 .andExpect(status().isOk())
                 .andReturn();
         assertNotNull(result);
@@ -59,15 +66,50 @@ public class MockMvcIntegrationTest {
         var response = result.getResponse();
         assertNotNull(response);
         String json = response.getContentAsString();
-        assertFalse(StringUtils.isBlank(json));
+        assertTrue(StringUtils.isNotBlank(json));
 
-        List<PersonDTO> dtos = parseListOfPeopleDtos(json);
+        List<PersonDTO> dtos = OBJECT_MAPPER.readValue(json, listType());
         assertNotNull(dtos);
         assertTrue(dtos.isEmpty());
     }
 
-    private static List<PersonDTO> parseListOfPeopleDtos(String json) throws JsonProcessingException {
-        CollectionType collectionType = OBJECT_MAPPER.getTypeFactory().constructCollectionType(List.class, PersonDTO.class);
-        return OBJECT_MAPPER.readValue(json, collectionType);
+    // Note that this gets us 403 while the TestRestTemplate version gets us 401
+    @Disabled("For some reason, this gives 403 UNAUTHORIZED even though it has the same headers as the GET request above")
+    @WithMockUser(username = USERNAME, password = PASSWORD)
+    @DisplayName("can create a Person")
+    @Test
+    void testCreatePerson() throws Exception {
+        PersonDTO personDto = MockUtil.mockCreatePersonDto();
+
+        var result = mockMvc.perform(
+                        MockMvcRequestBuilders.post(URI.create("/api/person"))
+                                .content(OBJECT_MAPPER.writeValueAsString(personDto))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+        assertNotNull(result);
+
+        var response = result.getResponse();
+        assertNotNull(response);
+        String json = response.getContentAsString();
+        assertTrue(StringUtils.isNotBlank(json));
+
+        ResponseEntity<PersonDTO> responseEntity = OBJECT_MAPPER.readValue(json, responseEntityType());
+        assertNotNull(responseEntity);
+        PersonDTO dto = responseEntity.getBody();
+        assertNotNull(dto);
+        assertNotNull(dto.getUuid());
+    }
+
+    private static <T> TypeReference<ResponseEntity<T>> responseEntityType() {
+        return new TypeReference<>() {
+        };
+    }
+
+    private static <T> TypeReference<List<T>> listType() {
+        return new TypeReference<>() {
+        };
     }
 }
